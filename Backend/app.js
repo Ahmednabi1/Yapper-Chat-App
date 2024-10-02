98fc874e4f8b8130337f47ae7dded2e04070e64f
Abd_elrhman <aeledeisy5@gmail.com>
2024-10-02 13:37:15 +0300

    added ChatRooms front and Backend

--- Backend/app.js
+++ Backend/app.js
@@ -1,48 +1,47 @@
-require('dotenv').config()
-const express = require('express');
-const dotenv = require('dotenv');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser')
-const session = require('express-session')
-const http = require('http');
-const socketIO = require('socket.io');
-const cors = require('cors');
-const connectDB = require('./config/database');
-const passport = require('./config/passport');
-const authRoutes = require('./routes/auth');
+require("dotenv").config();
+const express = require("express");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const http = require("http");
+const cors = require("cors");
+const connectDB = require("./config/database");
+const passport = require("./config/passport");
+const authRoutes = require("./routes/auth");
+const socketController = require("./controllers/socketController");
+const corsMiddleware = require("./middleware/corsMiddleware");
 
 dotenv.config();
 const app = express();
 const server = http.createServer(app);
-const io = socketIO(server);
 
-connectDB();
-
-app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(
-    session({
-        resave: false,
-        saveUninitialized: false,
-        secret: 'your-secret-key',
-        cookie: {
-            secure: false,
-        }
-    })
-)
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: "your-secret-key",
+    cookie: {
+      secure: false,
+    },
+  })
+);
 
 app.use(passport.initialize());
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
+app.get("");
 
-
+socketController(server);
+corsMiddleware(app);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port `);
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 //http://localhost:5000/api/auth/facebook
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,17 @@
-import LoginForm from './components/LoginForm/LoginForm';
-import RegisterForm from './components/RegisterForm/RegisterForm';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from "react";
+import LoginForm from "./components/LoginForm/LoginForm";
+import RegisterForm from "./components/RegisterForm/RegisterForm";
+import ChatRooms from "./components/ChatRooms/ChatRooms";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/login" element={<LoginForm />} />
+          <Route path="/" element={<LoginForm />} />
           <Route path="/register" element={<RegisterForm />} />
+          <Route path="/chat" element={<ChatRooms />} />
         </Routes>
       </div>
     </Router>
