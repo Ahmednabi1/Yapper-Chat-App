248e408d8d052f11562968d4210cc9501dbb3438
Sarana19 <123037658+Sarana19@users.noreply.github.com>
2024-10-06 21:34:39 +0100

    Update App.js
    Add profile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import LoginForm from "./components/LoginForm/LoginForm";
 import RegisterForm from "./components/RegisterForm/RegisterForm";
 import ChatRooms from "./components/ChatRooms/ChatRooms";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import ProfileForm from "./components/Profile/porfile";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/login" element={<LoginForm />} />
           <Route path="/register" element={<RegisterForm />} />
           <Route path="/chat" element={<ChatRooms />} />
+          <Route path="/profile" element={<ProfileForm />} />
         </Routes>
       </div>
     </Router>
