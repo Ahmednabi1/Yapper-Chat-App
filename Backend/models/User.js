cbb615757a949d114c3f7c4c2561f241e6752447
Sarana19 <123037658+Sarana19@users.noreply.github.com>
2024-10-16 19:18:16 +0100

    Update User.js

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -12,11 +12,11 @@ const userSchema = new mongoose.Schema({
     },
     username: {
         type: String,
-        required: true,
+       
     },
      phone: {
         type: String,
-        required: true,
+        
     },
      profilePicture: {
         type: String,  
