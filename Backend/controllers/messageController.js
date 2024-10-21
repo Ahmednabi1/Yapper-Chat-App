4e2d3ac3b61da6186e068af15abb56f5a618e141
Abdelrahman Magdy Mohammed <46284126+bid00@users.noreply.github.com>
2024-10-21 18:54:17 +0300

    Update messageController.js
    Comments

--- Backend/controllers/messageController.js
+++ Backend/controllers/messageController.js
@@ -1,11 +1,11 @@
 const Message = require("../models/Messages");
-
+//getting messages from the database 
 const getMessages = async (req, res) => {
     const roomId = req.params.roomId;
 
     try {
-        const messages = await Message.find({ chatroom: roomId })
-        res.json(messages);
+        const messages = await Message.find({ chatroom: roomId }) //find the messages from the database where chatroom = roomid
+        res.json(messages); //return the messages in the response
 
     } catch (error) {
         console.error('Error fetching messages:', error);
